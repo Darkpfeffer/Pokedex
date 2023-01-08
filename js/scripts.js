c7ae48d1a870f09608275899834b50c141355453
Darkpfeffer <darkpfeffer97@gmail.com>
2023-01-08 13:27:31 +0100

    Added pokemon1,2,3 to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+// reference: https://pokedex.org/
 let pokemonList= [];
+let pokemon1= {
+    name: 'Bulbasaur',
+    heightCm: 70,
+    types: ['grass', 'poison']
+}
+let pokemon2= {
+    name: 'Charmander',
+    heightCm: 60,
+    types: ['fire'],
+}
+let pokemon3= {
+    name: 'Squirtle'
+    heightCm: 50,
+    types: ['water']
+}
+
+pokemonList= [pokemon1, pokemon2, pokemon3]
