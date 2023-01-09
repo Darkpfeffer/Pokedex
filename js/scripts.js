2ed14023f5368280fb67cff8b80b29ce22367fce
Darkpfeffer <darkpfeffer97@gmail.com>
2023-01-09 01:39:47 +0100

    created conditional to pokémonlist loop

--- js/scripts.js
+++ js/scripts.js
@@ -23,5 +23,9 @@ pokemonList= [pokemon1, pokemon2, pokemon3];
 
 // Loop to write a list of the pokemons' name and height.
 for (let i= 0; i< pokemonList.length; i++) {
+    if(pokemonList[i].height > 65) {
+        document.write('<p>'+ '<span class=pokemon-name>Name: </span>'+ pokemonList[i].name+ '<span class=pokemon-height> Height: </span>'+ pokemonList[i].height+ ' - Wow, that\'s big'+ '</p>')
+    } else {
     document.write('<p>'+ '<span class=pokemon-name>Name: </span>'+ pokemonList[i].name+ '<span class=pokemon-height> Height: </span>'+ pokemonList[i].height+ '</p>')
+    }
 }
