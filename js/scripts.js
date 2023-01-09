a1b3fcf4d0bfd2b5a4c9a0f79dbae8ebc26bac51
Darkpfeffer <darkpfeffer97@gmail.com>
2023-01-09 01:21:45 +0100

    make notes 'scripts.js'

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // reference: https://pokedex.org/
 let pokemonList= [];
 
+// Database for pokémons
 let pokemon1= {
     name: 'Bulbasaur',
     // height in cm
@@ -20,6 +21,7 @@ let pokemon3= {
 
 pokemonList= [pokemon1, pokemon2, pokemon3];
 
+// Loop to write a list of the pokemons' name and height.
 for (let i= 0; i< pokemonList.length; i++) {
     document.write('<p>'+ 'Name: '+ pokemonList[i].name+ ', Height: '+ pokemonList[i].height+ '</p>')
 }
