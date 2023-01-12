8b6f3bd1effef455a460b07672666c2d1d168534
Darkpfeffer <darkpfeffer97@gmail.com>
2023-01-12 20:01:48 +0100

    restricted 'pokemonRepository.add' function
    only objects with specific names are addable to 'pokemonList'

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,19 @@
 let pokemonRepository= (function() {
     let pokemonList= [];
 
+    let obj= {name: 'pokemon', height: 70, types: ['fire']};
+
     // push pokemon data to 'pokemonList'
     function add(pokemon) {
-        if(typeof pokemon=== 'object') {
+        if(
+            typeof pokemon=== 'object' &
+            typeof pokemon.name=== 'string' &
+            typeof pokemon.height=== 'number' &
+            Array.isArray(pokemon.types)
+        ) {
             pokemonList.push(pokemon)
         } else {
-            console.log(`It's not an object`)
+            console.log(`Not valid!`)
         }
     }
   
@@ -26,7 +33,6 @@ let pokemonRepository= (function() {
 pokemonRepository.add({name: 'Bulbasaur', height: 70, types: [' grass', ' poison']});
 pokemonRepository.add({name: 'Charmander', height: 60, types: ['fire']});
 pokemonRepository.add({name: 'Squirtle', height: 50, types: ['water']});
-
 
 // Loop to write a list of the pokemons' name and height.
   
