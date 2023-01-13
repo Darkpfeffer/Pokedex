75b2d9469515c4c973b2ad7218f94c69ef7bd126
Darkpfeffer <darkpfeffer97@gmail.com>
2023-01-13 12:34:32 +0100

    delete unused codes 'scripts.js'

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@
 let pokemonRepository= (function() {
     let pokemonList= [];
 
-    let obj= {name: 'pokemon', height: 70, types: ['fire']};
-
     // push pokemon data to 'pokemonList'
     function add(pokemon) {
         if(
@@ -58,47 +56,3 @@ pokemonRepository.getAll().forEach(function(pokemon) {
             '</p>')
     }
 })
-
-/* old code before IIFE
-
-// reference: https://pokedex.org/
-let pokemonList= [];
-
-// Database for pokémons
-let pokemon1= {
-    name: 'Bulbasaur',
-    // height in cm
-    height: 70,
-    types: ['grass', 'poison']
-};
-let pokemon2= {
-    name: 'Charmander',
-    height: 60,
-    types: ['fire'],
-};
-let pokemon3= {
-    name: 'Squirtle',
-    height: 50,
-    types: ['water']
-};
-
-pokemonList= [pokemon1, pokemon2, pokemon3];
-
-// Loop to write a list of the pokemons' name and height.
-pokemonList.forEach(function(pokemon) {
-    if(pokemon.height > 65) {
-        document.write('<p>'+ 
-        '<span class=pokemon-name>Name: </span>'+ 
-        pokemon.name+ '<span class=pokemon-height> Height: </span>'+ 
-        pokemon.height+ 
-        ' - Wow, that\'s big'+ 
-        '</p>')
-    } else {
-        document.write('<p>'+
-         '<span class=pokemon-name>Name: </span>'+ 
-         pokemon.name+ 
-         '<span class=pokemon-height> Height: </span>'+
-          pokemon.height+ 
-          '</p>')
-    }
-}); */
