bb74b5084358f03d06158603b890b25fa82ff2b2
Darkpfeffer <darkpfeffer97@gmail.com>
2023-01-30 21:14:16 +0100

    styled buttons

--- js/scripts.js
+++ js/scripts.js
@@ -33,10 +33,69 @@ let pokemonRepository= (function() {
                 let itemName= itemType.name;
                 buttonSelect.classList.add(itemName);
                 if (buttonSelect.classList.contains('grass') && buttonSelect.classList.contains('poison')) {
-                    // console.log('Type '+ item.slot+ '= '+ itemName)
-                    buttonSelect.classList.remove('grass')
-                    buttonSelect.classList.remove('poison')
-                    buttonSelect.classList.add('grassPoison') 
+                    buttonSelect.classList.remove('grass');
+                    buttonSelect.classList.remove('poison');
+                    buttonSelect.classList.add('grassPoison');
+                }else if (buttonSelect.classList.contains('bug') && buttonSelect.classList.contains('poison')) {
+                    buttonSelect.classList.remove('bug');
+                    buttonSelect.classList.remove('poison');
+                    buttonSelect.classList.add('bugPoison')
+                }else if (buttonSelect.classList.contains('ground') && buttonSelect.classList.contains('poison')) {
+                    buttonSelect.classList.remove('ground');
+                    buttonSelect.classList.remove('poison');
+                    buttonSelect.classList.add('poisonGround')
+                }else if (buttonSelect.classList.contains('normal') && buttonSelect.classList.contains('fairy')){
+                    buttonSelect.classList.remove('normal');
+                    buttonSelect.classList.remove('fairy');
+                    buttonSelect.classList.add('normalFairy')
+                }else if (buttonSelect.classList.contains('bug') && buttonSelect.classList.contains('grass')){
+                    buttonSelect.classList.remove('bug');
+                    buttonSelect.classList.remove('grass');
+                    buttonSelect.classList.add('grassBug')
+                }else if (buttonSelect.classList.contains('water') && buttonSelect.classList.contains('poison')){
+                    buttonSelect.classList.remove('water');
+                    buttonSelect.classList.remove('poison');
+                    buttonSelect.classList.add('poisonWater')
+                }else if (buttonSelect.classList.contains('rock') && buttonSelect.classList.contains('ground')){
+                    buttonSelect.classList.remove('rock');
+                    buttonSelect.classList.remove('ground');
+                    buttonSelect.classList.add('groundRock')
+                }else if (buttonSelect.classList.contains('water') && buttonSelect.classList.contains('psychic')){
+                    buttonSelect.classList.remove('water');
+                    buttonSelect.classList.remove('psychic');
+                    buttonSelect.classList.add('psychicWater')
+                }else if (buttonSelect.classList.contains('electric') && buttonSelect.classList.contains('steel')){
+                    buttonSelect.classList.remove('electric');
+                    buttonSelect.classList.remove('steel');
+                    buttonSelect.classList.add('electricSteel')
+                }else if (buttonSelect.classList.contains('water') && buttonSelect.classList.contains('ice')){
+                    buttonSelect.classList.remove('water');
+                    buttonSelect.classList.remove('ice');
+                    buttonSelect.classList.add('iceWater')
+                }else if (buttonSelect.classList.contains('ghost') && buttonSelect.classList.contains('poison')){
+                    buttonSelect.classList.remove('ghost');
+                    buttonSelect.classList.remove('poison');
+                    buttonSelect.classList.add('poisonGhost')
+                }else if (buttonSelect.classList.contains('grass') && buttonSelect.classList.contains('psychic')){
+                    buttonSelect.classList.remove('grass');
+                    buttonSelect.classList.remove('psychic');
+                    buttonSelect.classList.add('psychicGrass')
+                }else if (buttonSelect.classList.contains('psychic') && buttonSelect.classList.contains('fairy')){
+                    buttonSelect.classList.remove('psychic');
+                    buttonSelect.classList.remove('fairy');
+                    buttonSelect.classList.add('psychicFairy')
+                }else if (buttonSelect.classList.contains('ice') && buttonSelect.classList.contains('psychic')){
+                    buttonSelect.classList.remove('ice');
+                    buttonSelect.classList.remove('psychic');
+                    buttonSelect.classList.add('psychicIce')
+                }else if (buttonSelect.classList.contains('rock') && buttonSelect.classList.contains('water')){
+                    buttonSelect.classList.remove('rock');
+                    buttonSelect.classList.remove('water');
+                    buttonSelect.classList.add('rockWater')
+                }else if (buttonSelect.classList.contains('flying')) {
+                    buttonSelect.classList.remove('flying');
+                }else {
+                    console.log('classification complete')
                 }
             })
         })
@@ -49,7 +108,7 @@ let pokemonRepository= (function() {
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function() {
-            console.log(pokemon);
+            console.log('Name: '+ pokemon.name+ ' Height: '+ pokemon.height+ ' imgUrl: '+ pokemon.imageUrl);
         });
     }
 
